9871894ad7e3c81cfb6f077c516ff1b2e5557c20
BB-choi <choibobae0809@gmail.com>
2022-05-25 16:50:14 +0900

    design: header 영역 배경 스타일

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,6 +2,8 @@ import { Button, Grid } from "@mui/material";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 
+import heroImage from "assets/hero-img.png";
+
 import GNB from "./GNB/GNB";
 
 const LogoArea = () => {
@@ -33,13 +35,16 @@ const UserMenu = () => {
 const indexHeaderStyle = {
   maxwidth: "1440px",
   height: "640px",
-  backgroundColor: "green",
+  backgroundImage: `url(${heroImage})`,
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center bottom",
+  backgroundSize: "cover",
 };
 
 const Header = () => {
   return (
     <Box component="header" sx={indexHeaderStyle}>
-      <Container maxWidth="xl" sx={{ height: "100px" }}>
+      <Container component="header" maxWidth="xl" sx={{ height: "100px" }}>
         <Grid container spacing={2} columns={12}>
           <LogoArea />
           <GNB />
