ce94897445a56d5f43c6ef9eaa1e5c474e98aad9
BB-choi <choibobae0809@gmail.com>
2022-05-26 14:32:47 +0900

    refactor: GNB props 이름 변경

--- frontend/src/components/Header/GNB/GNB.tsx
+++ frontend/src/components/Header/GNB/GNB.tsx
@@ -2,10 +2,10 @@ import { ButtonGroup, Grid, GridProps } from "@mui/material";
 
 import NavItem from "./NavItem";
 
-const GNB = ({ menuData, ...args }: GNBProps): JSX.Element => {
+const GNB = ({ menuData, ...MUIGridProps }: GNBProps): JSX.Element => {
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
-    <Grid {...args}>
+    <Grid {...MUIGridProps}>
       <ButtonGroup component="nav" aria-label="메인 메뉴">
         {menuData.map(({ id, name }) => (
           <NavItem key={id} item={name} />
