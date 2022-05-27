230ec4c2343fdacac817308d4ce93cb20341550a
BB-choi <choibobae0809@gmail.com>
2022-05-27 12:15:46 +0900

    refactor: Buttons.style.ts String literal 이용하여 리팩토링

--- frontend/src/components/Header/SearchBar/SelectItemArea/ButtonArea/Buttons/Buttons.style.ts
+++ frontend/src/components/Header/SearchBar/SelectItemArea/ButtonArea/Buttons/Buttons.style.ts
@@ -1,36 +1,36 @@
 import { IconButton, IconButtonProps } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
-const RoundButton = styled(IconButton)<RoundButtonProps>(
-  ({ icon, theme: { palette, size } }) => `
-  width: ${
-    icon === "close"
-      ? size.searchBar.closeButton.width
-      : size.searchBar.searchButton.width
-  };
-  height: ${
-    icon === "close"
-      ? size.searchBar.closeButton.height
-      : size.searchBar.searchButton.height
-  };
-  background-color: ${
-    icon === "close" ? palette.grey4.main : palette.primary.main
-  };
-  margin-right: ${icon === "close" ? size.searchBar.closeButton.width : 0};
+type ButtonName = "closeButton" | "searchButton";
+const closeButton: ButtonName = "closeButton";
+const searchButton: ButtonName = "searchButton";
 
-  svg {
-    width: ${
-      icon === "close"
-        ? size.searchBar.closeButton.icon.width
-        : size.searchBar.searchButton.icon.width
+type ColorName = "grey4" | "primary" | "grey1" | "white";
+const grey1: ColorName = "grey1";
+const grey4: ColorName = "grey4";
+const primary: ColorName = "primary";
+const white: ColorName = "white";
+
+const RoundButton = styled(IconButton)<RoundButtonProps>(
+  ({ icon, theme: { palette, size } }) => {
+    const isCloseIcon = icon === "close";
+    const buttonKeyName = isCloseIcon ? closeButton : searchButton;
+    const bgColor = isCloseIcon ? grey4 : primary;
+    const svgColor = isCloseIcon ? grey1 : white;
+
+    return `  
+    width: ${size.searchBar[buttonKeyName].width};
+    height: ${size.searchBar[buttonKeyName].height};
+    background-color: ${palette[bgColor].main};
+    margin-right: ${isCloseIcon && size.searchBar.closeButton.width};
+
+    svg {
+      width: ${size.searchBar[buttonKeyName].icon.width};
+      height: ${size.searchBar[buttonKeyName].icon.height};
+      color: ${palette[svgColor].main};
     };
-    height: ${
-      icon === "close"
-        ? size.searchBar.closeButton.icon.height
-        : size.searchBar.searchButton.icon.height
-    };
-    color: ${icon === "close" ? palette.grey1.main : palette.white.main};
-  };`
+  `;
+  }
 );
 
 export default RoundButton;
