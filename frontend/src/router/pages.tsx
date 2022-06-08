54afe96c4beb7ba0b43e5191ee146a1bd914460c
BB-choi <choibobae0809@gmail.com>
2022-06-08 15:42:48 +0900

    fix: import-order 오류 해결

--- frontend/src/router/pages.tsx
+++ frontend/src/router/pages.tsx
@@ -1,7 +1,6 @@
 import Header from "components/Header/Header";
 import SearchResult from "components/Main/SearchResult/SearchResult";
 import NotFound from "components/NotFound/NotFound";
-
 import { LinkPath } from "router";
 
 // TODO: searchResult 파라미터가 없을때 올바르지 않다고 표시되도록?
